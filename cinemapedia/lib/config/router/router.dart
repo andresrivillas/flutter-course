91c66f10fe0c8321268669db268a507802ba209f
Andres Rivillas <arivillasvelez@carnival.com>
2026-06-01 16:35:45 -0500

    Router

--- cinemapedia/lib/config/router/router.dart
+++ cinemapedia/lib/config/router/router.dart
@@ -1,20 +1,36 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/movies/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/movies/home_view.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: "/",
   routes: [
-    GoRoute(
-      path: "/",
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: "movie/:id",
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
+            return HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: "movie/:id",
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return FavoritesView();
           },
         ),
       ],
---
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,54 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomAppbar extends ConsumerWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final colors = Theme.of(context).colorScheme;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(Icons.movie_outlined, color: colors.primary),
+              SizedBox(width: 5),
+              Text("Cinemapedia", style: titleStyle),
+              Spacer(),
+              IconButton(
+                onPressed: () {
+                  final searchQuery = ref.read(searchedQueryProvider);
+                  final searchedMovies = ref.read(searchedMoviesProvider);
+
+                  showSearch<Movie?>(
+                    query: searchQuery,
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      initialMovies: searchedMovies,
+                      searchmovies: ref
+                          .read(searchedMoviesProvider.notifier)
+                          .searchMoviesByQuery,
+                    ),
+                  ).then((movie) {
+                    if (movie == null) return;
+                    context.push("/movie/${movie.id}");
+                  });
+                },
+                icon: Icon(Icons.search),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
